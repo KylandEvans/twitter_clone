45c3b6db3a8004679b70de179ed260bfafae5143
Kyland Evans <Kylandevans@gmail.com>
2022-09-11 15:28:02 -0400

    first commit - home page styles done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const engine = require("ejs-mate");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine("ejs", engine);
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.listen(port, console.log(`Listening on port ${port}`));
